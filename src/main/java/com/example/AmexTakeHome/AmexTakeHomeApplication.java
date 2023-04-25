ecc1f034d5d4d49de3ac30e3d902cd9d1f54a955
A Sherman <aliannea.sherman@gmail.com>
2023-04-24 19:56:24 -0700

    initialize spring boot project

---
+++ src/main/java/com/example/AmexTakeHome/AmexTakeHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.AmexTakeHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmexTakeHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmexTakeHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AmexTakeHome/AmexTakeHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AmexTakeHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmexTakeHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
